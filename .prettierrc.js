06e382be9c20a9a5c80de300873577d39d242f91
Son Nguyen <snguyenvie@gmail.com>
2021-02-11 10:57:19 +0700

    init

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+};
---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (<View><Text>Home</Text></View>);
+};
+
+export default App;
